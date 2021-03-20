d6ca32a65bcc6503e1a352981ca6028dfc0dcc07
Fred <frederick.ries@aktome.fr>
2021-03-20 22:45:03 +0100

    Adding MessageHandler

---
+++ src/BeeBus.Core/IMessageBus.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeeBus
+{
+    
+    public interface IMessageBus
+    {
+        Task SendAsync<TMessage>(TMessage command, CancellationToken cancellation) where TMessage : IMessage;
+
+        Task<TMessageResponse> SendAsync<TMessage, TMessageResponse>(TMessage command, CancellationToken cancellation) where TMessage : IMessage<TMessageResponse>;
+    }
+}
---
+++ src/BeeBus.Core/IMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeeBus
+{
+    
+    public interface IMessageHandler
+    {
+        Task HandleAsync<TMessage>(TMessage command, CancellationToken cancellation) where TMessage : IMessage;
+
+        Task<TMessageResponse> HandleAsync<TMessage, TMessageResponse>(TMessage command, CancellationToken cancellation) where TMessage : IMessage<TMessageResponse>;
+    }
+}
