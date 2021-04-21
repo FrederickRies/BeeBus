f58efc65ad26fd7c8cb7c4dc6d3b5f02aa38abb7
Fred <17268332+FrederickRies@users.noreply.github.com>
2021-04-21 16:16:28 +0200

    Adding handler delegate to addressing the middleware approach

--- src/BeeBus.Core/IMessageBus.cs
+++ src/BeeBus.Core/IMessageBus.cs
@@ -15,7 +15,7 @@ namespace BeeBus
         /// <exception cref="InvalidOperationException">Thrown when no handler is found to handle the message.</exception>
         /// <typeparam name="TMessage">The type of the message to handle.</typeparam>
         /// <param name="message">The message to handle.</param>
-        /// <param name="cancellation"></param>
+        /// <param name="cancellation">The token to monitor for cancellation requests.</param>
         /// <returns>A <see cref="Task"/> that indiquate the completion of the handling of the message.</returns>
         Task SendAsync<TMessage>(TMessage message, CancellationToken cancellation) where TMessage : IMessage;
 
@@ -26,7 +26,7 @@ namespace BeeBus
         /// <typeparam name="TMessage">The type of the message to handle.</typeparam>
         /// <typeparam name="TMessageResponse">The type of the response awaited.</typeparam>
         /// <param name="message">The message to handle.</param>
-        /// <param name="cancellation"></param>
+        /// <param name="cancellation">The token to monitor for cancellation requests.</param>
         /// <returns>A <see cref="Task"/> that on completion returns the associated response for the message.</returns>
         Task<TMessageResponse> SendAsync<TMessage, TMessageResponse>(TMessage message, CancellationToken cancellation) where TMessage : IMessage<TMessageResponse>;
     }
---
+++ src/BeeBus.Core/RequestDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeeBus.Core
+{
+    /// <summary>
+    /// A function that can process a message
+    /// </summary>
+    /// <typeparam name="TMessage">The type of the message to handle.</typeparam>
+    /// <param name="message">The message to handle.</param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+    /// <returns></returns>
+    public delegate Task RequestDelegate<in TMessage>(TMessage message, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// A function that can process a message and produce a response
+    /// </summary>
+    /// <typeparam name="TMessage">The type of the message to handle.</typeparam>
+    /// <typeparam name="TResponse">The type of the response awaited.</typeparam>
+    /// <param name="message">The message to handle.</param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+    /// <returns></returns
+    public delegate Task<TResponse> RequestDelegate<in TMessage, TResponse>(TMessage message, CancellationToken cancellationToken);
+}
