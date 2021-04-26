a439a476af799dea82e57d7a47c4d00a8ae8aed6
Fred <17268332+FrederickRies@users.noreply.github.com>
2021-04-26 23:43:23 +0200

    Middleware delegate

---
+++ src/BeeBus.Core/HandlerMiddlewareDelegate.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeeBus.Core
+{
+    public delegate Task RequestMiddlewareDelegate<TMessage>(TMessage message, HandlerDelegate<TMessage> next, CancellationToken cancellationToken);
+
+    public delegate Task<TResponse> RequestMiddlewareDelegate<TMessage, TResponse>(TMessage request, HandlerDelegate<TMessage, TResponse> next, CancellationToken cancellationToken);
+}
