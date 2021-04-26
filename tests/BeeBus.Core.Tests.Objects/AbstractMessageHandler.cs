3ab8b8c9ad75ab4510b4ee5044e530213d296c7a
Fred <17268332+FrederickRies@users.noreply.github.com>
2021-04-26 23:51:17 +0200

    Fix little variable name inconsistance

--- tests/BeeBus.Core.Tests.Objects/AbstractMessageHandler.cs
+++ tests/BeeBus.Core.Tests.Objects/AbstractMessageHandler.cs
@@ -5,7 +5,7 @@ namespace BeeBus.Core
 {
     public abstract class AbstractMessageHandler : IMessageHandler<AbstractMessage>
     {
-        public Task HandleAsync(AbstractMessage message, CancellationToken cancellation)
+        public Task HandleAsync(AbstractMessage message, CancellationToken cancellationToken)
         {
             message.Handled = true;
             return Task.CompletedTask;
